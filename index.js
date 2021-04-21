7c167017aaedf488a303b223871cbc70d10cc8d1
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-21 19:47:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const event="surprise";*/
 const names=['Liza','Kaitlin','Jan'];
   function writeCards(names, event) {
     let newArray=[];
-    event="surprise;"
+    event="surprise";
   for (let i=0; i<names.length; i++) {
   newArray.push (`Thank you, ${names[i]} , for the wonderful ${event} gift!`);
   }
