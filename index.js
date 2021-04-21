3eba971a312a88fafe3f32fe1c4dcba2a9bffe8d
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-21 19:44:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+/*const names=['Liza','Kaitlin','Jan'];
+const event="surprise";*/
+  function writeCards(names, event) {
+    let newArray=[];
+  for (let i=0; i<names.length; i++) {
+  newArray.push (`Thank you, ${names[i]} , for the wonderful ${event} gift!`);
+  }
+  return newArray
+}
+const names=['Liza','Kaitlin','Jan'];
+console.log (writeCards(names, "surprise"));
+
+
+
+let number = 10;
+function countDown(number) {
+  while (number>=0) {
+    console.log(number--);}
+}
+//console.log(countDown);
